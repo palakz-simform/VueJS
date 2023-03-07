5dd73761e0e9971f068cc35135feeb677c3c999e
Palak Zalavadia <palak.z@simformsolutions.com>
2023-03-07 18:40:32 +0530

    Navbar component added

---
+++ car-showroom/src/Nav-bar.vue
@@ -0,0 +1,27 @@
+<template>
+<div class="title">
+    <h1>Car Gallery</h1>
+    <hr>
+</div>
+
+</template>
+<script>
+</script>
+<style>
+.title {
+    text-align: center;
+}
+
+hr {
+    width: 8%;
+    border: 5px solid brown;
+    border-radius: 5px;
+}
+
+.title h1 {
+    color: black;
+    font-size: 40px;
+    text-shadow: 0 0 3px gray;
+}
+
+</style>
